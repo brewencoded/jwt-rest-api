445eb39fab93ab079fd7648fa01337e764a1775d
adplotter <victor@jbinternetholdings.com>
2016-05-31 16:12:03 -0500

    implemented compareHash in hash.js

--- api/auth/hash.js
+++ api/auth/hash.js
@@ -2,17 +2,30 @@ const bcrypt = require('bcrypt');
 
 module.exports = {
     createHash: function (text, saltRounds) {
-        const hashPromise = new Promise(function (resolve, reject) {
+        const hashPromise = new Promise(function (resolve) {
             bcrypt.hash(text, saltRounds, (err, hash) => {
                 if (err) {
                     resolve(err);
                 } else {
                     resolve(hash);
                 }
-            })
+            });
 
         });
 
         return hashPromise;
+    },
+    compareHash: function (text, hash) {
+        const comparePromise = new Promise(function (resolve) {
+            bcrypt.compare(text, hash, (err, res) => {
+                if (err) {
+                    resolve(err);
+                } else {
+                    resolve(res);
+                }
+            });
+        });
+
+        return comparePromise;
     }
-}
+};
--- test/unit/hashing.js
+++ test/unit/hashing.js
@@ -1,9 +1,12 @@
 const chai = require('chai'),
     expect = chai.expect,
+    chaiAsPromised = require('chai-as-promised'),
     hash = require('../../api/auth/hash.js'),
     bcrypt = require('bcrypt');
 
-describe('Password hashing', function () {
+chai.use(chaiAsPromised);
+
+describe('Text hashing', function () {
     const saltRounds = 10;
     const plainText = 'testString';
 
@@ -18,5 +21,29 @@ describe('Password hashing', function () {
             console.log(error);
             done();
         });
+    });
+});
+
+describe('Text comparison', function () {
+    let hashedText;
+    const saltRounds = 10;
+    const plainText = 'testString';
+
+    before(function (done) {
+        bcrypt.hash(plainText, saltRounds, (err, hash) => {
+            if (err) {
+                console.log(err);
+                done();
+            } else {
+                hashedText = hash;
+                done();
+            }
+        })
+    });
+
+    it('should return true if text and hash match', function () {
+        const comparison = hash.compareHash(plainText, hashedText)
+
+        return expect(comparison).to.eventually.be.true;
     });
 });
