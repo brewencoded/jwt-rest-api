c9041c5a5c9940ef54675b9f022cdc053d4f64bc
adplotter <victor@jbinternetholdings.com>
2016-05-31 16:33:05 -0500

    added api key pair creation files

---
+++ test/unit/keys.js
@@ -0,0 +1,8 @@
+const chai = require('chai'),
+    expect = chai.expect,
+    chaiAsPromised = require('chai-as-promised');
+
+describe('Api key generation', function () {
+    it('should return an object with two properties');
+    it('should have a property called id and key, of type string');
+});
